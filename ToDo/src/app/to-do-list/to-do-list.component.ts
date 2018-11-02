5bf7e9f153865f91209da961c53a7106e7fcc407
Mikel Princesa <mikelprincesa@gmail.com>
2018-11-01 22:13:48 -0400

    To Do List Component Displays Tasks

--- ToDo/src/app/to-do-list/to-do-list.component.ts
+++ ToDo/src/app/to-do-list/to-do-list.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./to-do-list.component.css']
 })
 export class ToDoListComponent implements OnInit {
-
+  title = "Tasks";
+  tasks = [{
+    taskId: 1,
+    taskName: "Meet with Patrice",
+    taskDate: "November 5, 2018",
+    taskLocation: "Slack" },
+    {
+      taskId: 2,
+      taskName: "Call Mom",
+      taskDate: "November 6, 2018",
+      taskLocation: "Cellphone" },
+    {
+    taskId: 3,
+    taskName: "Buy Pineapples",
+    taskDate: "November 8, 2018",
+    taskLocation: "WalMart" },
+    {
+    taskId: 4,
+    taskName: "Get 8 hours of sleep",
+    taskDate: "November 10, 2018",
+    taskLocation: "Home" },
+  ];
   constructor() { }
 
   ngOnInit() {
