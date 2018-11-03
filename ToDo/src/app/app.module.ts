9bc05e5c1d1e2acc26c69137c7b01fa787db9991
Mikel Princesa <mikelprincesa@gmail.com>
2018-11-03 17:32:45 -0400

    Routing Started/Format Changes

--- ToDo/src/app/app.module.ts
+++ ToDo/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { DetailsComponent } from './details/details.component';
+import { RouterModule, Routes} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    ToDoListComponent
+    ToDoListComponent,
+    DetailsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'details', component: DetailsComponent},
+      {path: 'app', component: AppComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
