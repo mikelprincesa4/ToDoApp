68fd01e9ea69e88a22433d4a431520d5c520e3cf
Mikel Princesa <mikelprincesa@gmail.com>
2018-11-06 20:00:02 -0500

    Details Component Appears Properly

--- ToDo/src/app/details/details.component.ts
+++ ToDo/src/app/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-details',
@@ -9,13 +10,14 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailsComponent implements OnInit {
   id: number;
   private sub: any;
+  tasks = [];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private taskService: TasksService) { }
 
   ngOnInit() {
+    this.tasks = this.taskService.getTasks();
     this.sub = this.route.params.subscribe(params => {
          this.id = +params['id']; // (+) converts string 'id' to a number
-      });
+    });
   }
-
 }
